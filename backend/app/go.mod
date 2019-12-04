ec78418bab2167c1597ce97e5284dc5f331e3758
ebkn <ktennis.mqekr12@gmail.com>
2019-12-05 02:50:57 +0900

    feat: run simple echo server with realize

--- backend/app/go.mod
+++ backend/app/go.mod
@@ -1,3 +1,10 @@
 module app
 
 go 1.13
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0
+	golang.org/x/crypto v0.0.0-20191202143827-86a70503ff7e // indirect
+)
--- backend/app/main.go
+++ backend/app/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	elog "github.com/labstack/gommon/log"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.NoContent(http.StatusOK)
+	})
+
+	e.Logger.SetLevel(elog.INFO)
+	e.HideBanner = true
+	e.Logger.Fatal(e.Start(":3000"))
+}
