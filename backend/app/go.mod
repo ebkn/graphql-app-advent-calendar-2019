c589ce4ad164ffc86cfbf716e59fb691710b1878
ebkn <ktennis.mqekr12@gmail.com>
2019-12-12 23:40:24 +0900

    feat: setup graphql handler

--- backend/app/go.mod
+++ backend/app/go.mod
@@ -3,8 +3,10 @@ module app
 go 1.13
 
 require (
+	github.com/99designs/gqlgen v0.10.2
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0
+	github.com/vektah/gqlparser v1.2.0
 	golang.org/x/crypto v0.0.0-20191202143827-86a70503ff7e // indirect
 )
--- backend/app/main.go
+++ backend/app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/99designs/gqlgen/handler"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	elog "github.com/labstack/gommon/log"
@@ -18,6 +19,7 @@ func main() {
 	e.GET("/health", func(c echo.Context) error {
 		return c.NoContent(http.StatusOK)
 	})
+	e.POST("/query", handler.GraphQL(NewExecutableSchema(Config{Resolvers: &Resolver{}})))
 
 	e.Logger.SetLevel(elog.INFO)
 	e.HideBanner = true
