32df878e227dbe2399698fbea122fa9ec8c61729
ebkn <ktennis.mqekr12@gmail.com>
2019-12-12 23:07:23 +0900

    feat: add unique identifier to tasks

--- backend/app/migrations/20191211172505_create_tasks.up.sql
+++ backend/app/migrations/20191211172505_create_tasks.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE tasks (
   id         INT NOT NULL AUTO_INCREMENT,
+  identifier varchar(255) BINARY NOT NULL,
   title      varchar(255) NOT NULL,
   notes      text NOT NULL,
   completed  tinyint(1) NOT NULL DEFAULT 0,
@@ -7,5 +8,6 @@ CREATE TABLE tasks (
   created_at timestamp NULL DEFAULT NULL,
   updated_at timestamp NULL DEFAULT NULL,
   deleted_at timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  UNIQUE KEY uix_tasks_identifier (identifier)
 ) ENGINE=InnoDB;
