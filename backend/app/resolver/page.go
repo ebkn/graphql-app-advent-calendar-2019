05301ac13a7eabb266e3adc6b43f6d7687536c6a
ebkn <ktennis.mqekr12@gmail.com>
2019-12-13 03:18:00 +0900

    feat: cursor, connection implementation

--- backend/app/resolver/page.go
+++ backend/app/resolver/page.go
@@ -67,36 +67,115 @@ func pageDB(db *gorm.DB, col string, dir direction, page model.PaginationInput) 
 	return db.Limit(limit), nil
 }
 
-func createCursor(resource string, id int, pKey *uint64) string {
+type cursorResource struct {
+	Name string
+	ID   int
+}
+
+func createCursor(first cursorResource, second *cursorResource) string {
 	var cursor []byte
-	if pKey != nil {
-		cursor = []byte(fmt.Sprintf("%s:%d:%d", resource, id, *pKey))
+	if second != nil {
+		cursor = []byte(fmt.Sprintf("%s:%d:%s:%d", first.Name, first.ID, second.Name, second.ID))
 	} else {
-		cursor = []byte(fmt.Sprintf("%s:%d", resource, id))
+		cursor = []byte(fmt.Sprintf("%s:%d", first.Name, first.ID))
 	}
 
 	return base64.StdEncoding.EncodeToString(cursor)
 }
 
-func decodeCursor(cursor string) (id string, pKey *int, err error) {
+func decodeCursor(cursor string) (cursorResource, *cursorResource, error) {
 	bytes, err := base64.StdEncoding.DecodeString(cursor)
 	if err != nil {
-		return "", nil, err
+		return cursorResource{}, nil, err
 	}
 
 	vals := strings.Split(string(bytes), ":")
 
 	switch len(vals) {
 	case 2:
-		return vals[1], nil, nil
-	case 3:
-		pKey, err := strconv.Atoi(vals[2])
+		id, err := strconv.Atoi(vals[1])
 		if err != nil {
-			return "", nil, errors.New("invalid_cursor")
+			return cursorResource{}, nil, errors.New("invalid_cursor")
 		}
 
-		return vals[1], &pKey, nil
+		return cursorResource{Name: vals[0], ID: id}, nil, nil
+	case 4:
+		id, err := strconv.Atoi(vals[1])
+		if err != nil {
+			return cursorResource{}, nil, errors.New("invalid_cursor")
+		}
+
+		id2, err := strconv.Atoi(vals[3])
+		if err != nil {
+			return cursorResource{}, nil, errors.New("invalid_cursor")
+		}
+
+		return cursorResource{
+				Name: vals[0],
+				ID:   id,
+			}, &cursorResource{
+				Name: vals[2],
+				ID:   id2,
+			}, nil
 	default:
-		return "", nil, errors.New("invalid_cursor")
+		return cursorResource{}, nil, errors.New("invalid_cursor")
 	}
+}
+
+func convertToConnection(tasks []*model.Task, orderBy model.TaskOrderFields, page model.PaginationInput) *model.TaskConnection {
+	if len(tasks) == 0 {
+		return &model.TaskConnection{PageInfo: &model.PageInfo{}}
+	}
+
+	pageInfo := model.PageInfo{}
+	if page.First != nil {
+		if len(tasks) >= *page.First+1 {
+			pageInfo.HasNextPage = true
+			tasks = tasks[:len(tasks)-1]
+		}
+	}
+
+	switch orderBy {
+	case model.TaskOrderFieldsLatest:
+		taskEdges := make([]*model.TaskEdge, len(tasks))
+
+		for i, task := range tasks {
+			cursor := createCursor(
+				cursorResource{Name: "task", ID: task.ID},
+				nil,
+			)
+			taskEdges[i] = &model.TaskEdge{
+				Cursor: cursor,
+				Node:   task,
+			}
+		}
+
+		pageInfo.EndCursor = taskEdges[len(taskEdges)-1].Cursor
+
+		return &model.TaskConnection{PageInfo: &pageInfo, Edges: taskEdges}
+	case model.TaskOrderFieldsDue:
+		taskEdges := make([]*model.TaskEdge, 0, len(tasks))
+
+		for _, task := range tasks {
+			if task.Due == nil {
+				return &model.TaskConnection{PageInfo: &pageInfo, Edges: taskEdges}
+			}
+
+			cursor := createCursor(
+				cursorResource{Name: "task", ID: int((*task.Due).Unix())},
+				&cursorResource{Name: "due", ID: task.ID},
+			)
+
+			taskEdges = append(taskEdges, &model.TaskEdge{
+				Cursor: cursor,
+				Node:   task,
+			})
+		}
+
+		pageInfo.EndCursor = taskEdges[len(taskEdges)-1].Cursor
+
+		return &model.TaskConnection{PageInfo: &pageInfo, Edges: taskEdges}
+	}
+
+	return &model.TaskConnection{PageInfo: &model.PageInfo{}}
 }
