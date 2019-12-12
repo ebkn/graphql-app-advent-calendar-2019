f2f309d7988e2f16b9bbf2e575f853c9ebe97c35
ebkn <ktennis.mqekr12@gmail.com>
2019-12-13 01:12:34 +0900

    feat: create task mutation

--- backend/app/resolver/resolver.go
+++ backend/app/resolver/resolver.go
@@ -3,6 +3,7 @@
 package resolver
 
 import (
+	"app/config"
 	"app/model"
 	"context"
 )
@@ -29,8 +30,27 @@ func (r *queryResolver) Tasks(ctx context.Context, input model.TasksInput, order
 }
 
 func (r *mutationResolver) CreateTask(ctx context.Context, input model.CreateTaskInput) (*model.Task, error) {
-	panic("not implemented")
+	db := config.DB()
+
+	id, err := config.ShortID().Generate()
+	if err != nil {
+		return &model.Task{}, err
+	}
+
+	task := model.Task{
+		Identifier: id,
+		Title:      input.Title,
+		Notes:      input.Notes,
+		Completed:  input.Completed,
+		Due:        input.Due,
+	}
+	if err := db.Create(&task).Error; err != nil {
+		return &model.Task{}, err
+	}
+
+	return &task, nil
 }
+
 func (r *mutationResolver) UpdateTask(ctx context.Context, input model.UpdateTaskInput) (*model.Task, error) {
 	panic("not implemented")
 }
